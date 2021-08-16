0e563045804b764d50df1293913216ddb7868ff9
rutger <rutbelt@gmail.com>
2021-08-16 22:16:35 +0200

    poll bot embed

--- clanbotjas/cogs/pollbot.py
+++ clanbotjas/cogs/pollbot.py
@@ -67,8 +67,9 @@ class PollEntry(object):
             height[indices] += 1
         argsorted = np.argsort(height)[::-1]
         string_builder = [f"{self.bars[i]}: {height[i]}" for i in argsorted]
-        return f"> {self.description}\n" + "\n".join(string_builder)
-        #
+        body = '\n'.join(string_builder)
+        return f"> {self.description}\n{body}"
+
 
     def results(self):
         height = np.zeros(len(self.bars), dtype=int)
@@ -107,6 +108,22 @@ class PollBot(commands.Cog):
                        guild_ids=settings.DISCORD_GUILD_IDS, options=options)
     async def _createpoll(self, ctx: SlashContext, description, max_values, **kwargs):
         entry = PollEntry(ctx.author, description, kwargs)
+        embed = discord.Embed(title="Sample Embed", url="https://realdrewdata.medium.com/",
+                              description="This is an embed that will show how to build an embed and the different components",
+                              color=0x109319)
+
+        # Add author, thumbnail, fields, and footer to the embed
+        embed.set_author(name="RealDrewData", url="https://twitter.com/RealDrewData",
+                         icon_url="https://pbs.twimg.com/profile_images/1327036716226646017/ZuaMDdtm_400x400.jpg")
+
+        embed.set_thumbnail(url="https://i.imgur.com/axLm3p6.jpeg")
+
+        embed.add_field(name="Field 1 Title", value="This is the value for field 1. This is NOT an inline field.",
+                        inline=False)
+        embed.add_field(name="Field 2 Title", value="It is inline with Field 3", inline=True)
+        embed.add_field(name="Field 3 Title", value="It is inline with Field 2", inline=True)
+
+        embed.set_footer(text="This is the footer. It contains text at the bottom of the embed")
 
         message = await ctx.send(content=entry.plot(), components=[entry.get_components(max_values),
                                                                    create_actionrow(
